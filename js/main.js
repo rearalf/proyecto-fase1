ee38e9fefb2a6f6191c07f63e61337e9ca37e182
Ricardo Alf <rear_recinos@hotmail.com>
2025-05-10 15:52:53 -0600

    feat: Agregada funcionalidad en la pantalla de contactos

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navbar = document.getElementById('mainNav');
 
-    function checkScroll() {
-        const scrollHeight = window.scrollY;
-        const viewportHeight = window.innerHeight;
-        if (scrollHeight > 0.50 * viewportHeight) {
-            navbar.classList.add('scrolled-navbar');
-        } else {
-            navbar.classList.remove('scrolled-navbar');
+    if (navbar) {
+        function checkScroll() {
+            const scrollHeight = window.scrollY;
+            const viewportHeight = window.innerHeight;
+            if (scrollHeight > 0.50 * viewportHeight) {
+                navbar.classList.add('scrolled-navbar');
+            } else {
+                navbar.classList.remove('scrolled-navbar');
+            }
         }
+
+        window.addEventListener('scroll', checkScroll);
+        window.addEventListener('load', checkScroll);
     }
 
-    window.addEventListener('scroll', checkScroll);
-    window.addEventListener('load', checkScroll);
 });
+
+const contactBtn = document.getElementById('contact-button')
+
+if (contactBtn) {
+    contactBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const message = document.getElementById('message').value.trim();
+
+        swal("Mensaje enviado", "Gracias por contactarnos, " + name + ".", "success");
+    })
+}
